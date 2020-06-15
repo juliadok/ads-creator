cac112d0b3808ca3f0355cf65cc6b7edd18dfc0c
Julia (via Travis CI) <juliadokolina@gmail.com>
2020-06-15 20:24:05 +0000

    Deploy 0.1.0

---
+++ precache-manifest.c9eed158f4c5366d00f389406fed1b89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafbc2e04f9aa918c663c907eec8c651",
+    "url": "https://juliadok.github.io/ads-creator/index.html"
+  },
+  {
+    "revision": "01858e7b2c9985e2b112",
+    "url": "https://juliadok.github.io/ads-creator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "feb9ffcd362b78828a4b",
+    "url": "https://juliadok.github.io/ads-creator/static/css/main.19b7a0ca.chunk.css"
+  },
+  {
+    "revision": "01858e7b2c9985e2b112",
+    "url": "https://juliadok.github.io/ads-creator/static/js/2.61b97f47.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "https://juliadok.github.io/ads-creator/static/js/2.61b97f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb9ffcd362b78828a4b",
+    "url": "https://juliadok.github.io/ads-creator/static/js/main.e7d22778.chunk.js"
+  },
+  {
+    "revision": "b3ce3d20b137c8e241e0",
+    "url": "https://juliadok.github.io/ads-creator/static/js/runtime-main.5dbbf231.js"
+  }
+]);
