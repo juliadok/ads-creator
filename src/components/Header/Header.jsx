2ee264e9eb0cb7baa7e17f42d64c3713df7e256f
juliadok <juliadokolina@gmail.com>
2020-06-16 17:04:03 +0200

    Fix header link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <Navbar variant="dark" bg="dark" expand="sm">
       <Container>
-        <Navbar.Brand href="/">
-          <b>Template</b>Creator
-        </Navbar.Brand>
+        <Link to="/">
+          <Navbar.Brand>
+            <b>Template</b>Creator
+          </Navbar.Brand>
+        </Link>
       </Container>
     </Navbar>
   );
